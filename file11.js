8ae38eb680c937e3a26947b2596846da84cf45bc
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-11 15:34:04 +0530

    update

--- file11.js
+++ file11.js
@@ -2,6 +2,11 @@
 //     return x + y;
 // };
 
-const add = (x, y) => x + y;
-const result = add(4, 5);
+// const add = (x, y) => x + y;
+// const result = add(4, 5);
+// console.log(result);
+
+
+const sqr = (a) => a * a;
+const result = sqr(5);
 console.log(result);
