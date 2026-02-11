3ed31d36ebff980b6f60bd082d62766c3e4362f1
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-11 16:10:02 +0530

    update

--- file13.js
+++ file13.js
@@ -1,3 +1,9 @@
+// let items = () => {
+//     return { name: "John" };
+// };
+// const result = items();
+// console.log(result);
+
 let items = () => ({ name: "John" });
 const result = items();
 console.log(result);
---
+++ file14.js
@@ -0,0 +1,9 @@
+const names = ["Vivek", "Shivam", "Aman"]
+// console.log(names);
+// console.log(names[0]);
+// names.push("Rahul");  //? push()
+// console.log(names);
+
+//? Spread Operator
+const arr = [...names, "Rahul"]
+console.log(arr)
