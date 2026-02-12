29b7d1ba72c66c4a61f4df446ce5d1a4ff432263
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-12 10:11:03 +0530

    update

--- file18.js
+++ file18.js
@@ -1,2 +1,9 @@
-const products = ["product1", "product2", "product3"];
+// const products = ["product1", "product2", "product3"];
+
+const products = [
+  { id: 1, name: "product1", desc: "desc1", price: 1000 },
+  { id: 2, name: "product2", desc: "desc2", price: 2000 },
+  { id: 3, name: "product3", desc: "desc3", price: 3000 },
+];
+
 products.forEach((product) => console.log(product));
