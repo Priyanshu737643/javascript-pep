267896bc7312e9a5fedfef2f8de50a2fe2fa3a17
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-12 10:30:29 +0530

    update

--- file18.js
+++ file18.js
@@ -1,9 +1,22 @@
 // const products = ["product1", "product2", "product3"];
 
 const products = [
-  { id: 1, name: "product1", desc: "desc1", price: 1000 },
-  { id: 2, name: "product2", desc: "desc2", price: 2000 },
-  { id: 3, name: "product3", desc: "desc3", price: 3000 },
+  { id: 1, name: "Product1", desc: "This is description 1", price: 100 },
+  { id: 2, name: "product2", desc: "This is description 2", price: 120 },
+  { id: 3, name: "Product3", desc: "This is description 3", price: 150 },
 ];
 
-products.forEach((product) => console.log(product));
+let cart = [];
+
+// products.forEach((product) => console.log(product));
+
+products.forEach((product) => {
+    product.qty = 1;
+    product.total = product.qty * product.price;
+    // cart.push(product)
+    cart = [...cart, product];
+});
+
+cart.forEach((c) => console.log(c));
+
+
