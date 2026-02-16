ab5ac4ce1cb94c6fe5de887abf5b9b8365289f85
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-16 10:13:21 +0530

    update

---
+++ part4/file33.js
@@ -0,0 +1,12 @@
+const student = {
+  name: "John",
+  age: 21,
+};
+
+// console.log(student.name)
+// console.log(student.age)
+
+//? Destructuring
+const { name, age } = student;
+console.log(name);
+console.log(age);
