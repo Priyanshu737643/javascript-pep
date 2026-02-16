dcd347f859be7f9779394476113117f810a68a2d
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-16 11:52:21 +0530

    update

---
+++ part4/file38.js
@@ -0,0 +1,24 @@
+//! Date
+
+// let d = new Date();
+// console.log(d)
+
+//? get methods
+// console.log(d.getDate());
+// console.log(d.getMonth());
+// console.log(d.getFullYear());
+// console.log(d.getHours());
+// console.log(d.getMinutes());
+// console.log(d.getSeconds());
+// console.log(d.getMilliseconds());
+// console.log(d.getDay());
+// console.log(d.getTime());  //* 1/1/1970
+// console.log(Date.now());  //* 1/1/1970
+
+//? set methods
+// let d2 = new Date(2026,0,1,10,10,10);
+// console.log(d2)
+
+let d = new Date("February 7, 2020 10:10:10");
+console.log(d);
+console.log(d.getMonth());
