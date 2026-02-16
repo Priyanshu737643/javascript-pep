816cd633250697f2288ec83096d3ee1dd3e0b659
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-16 10:36:34 +0530

    update

--- part4/file33.js
+++ part4/file33.js
@@ -1,12 +1,22 @@
 const student = {
   name: "John",
-  age: 21,
+    age: 21,
+    city: "Jalandhar",
 };
 
 // console.log(student.name)
 // console.log(student.age)
 
 //? Destructuring
-const { name, age } = student;
-console.log(name);
-console.log(age);
+// const { name, age } = student;  //* can give property name only
+// console.log(name);
+// console.log(age);
+
+//? assign new variable to name filed
+// const { name: userName } = student;
+// console.log(userName);
+
+//? if already city given in student, then it will take that by default
+const { city="Amritsar"} = student;
+console.log(city);
+
