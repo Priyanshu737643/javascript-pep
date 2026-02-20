550922156c83ac4040a869f83103e58226c69d42
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-20 11:07:08 +0530

    updated

--- part5/file51.js
+++ part5/file51.js
@@ -1,7 +1,8 @@
 //! fetching API
 
-const url = "https://jsonplaceholder.typicode.com/users";
+// const url = "https://jsonplaceholder.typicode.com/users";
 
+const url = "http://localhost:8080/"  //? server running on node js (index8.js)
 async function fetchData() {
     const response = await fetch(url)  //? this will always return string
     const result = await response.json()  //? converting to json format
